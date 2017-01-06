c27436a41367532fc9d0f05fff868db6df081e4f
Renato Santos <comercial@webstorebrasil.com>
2017-01-06 18:59:39 -0200

    Create Account Screen

--- Register/app/App.js
+++ Register/app/App.js
@@ -3,5 +3,16 @@ var React  = require('react/addons');
 var CreateAccountScreen  = require('./CreateAccountScreen.js');
 
 var App = React.createClass({
+	render: function() {
+		return (
+			
+			<div className="application-wrapper">
+				
+				<div className="application_routeHlandler">
+				   <CreateAccountScreen>
+				</div>
 
+			</div>
+		)
+	}
 });
