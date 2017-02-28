337cce21a96c4d6dfda0e505137e8232beca6fa2
Renato Santos <comercial@webstorebrasil.com>
2017-02-28 18:45:20 -0300

    add menu mobile

---
+++ MenuMobile/navigation.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Link } from 'react-from';
+
+export default class NavContainer extends Component {
+   render() {
+   	<div className="nav-container">
+   	<div className="site_title"><Link to="/">WEBSITE TITLE</link></div>
+   }
+}
+
+constructor(props) {
+  
+  super(props);
+  this.state {
+  	windowWidth: window.innerWidth,
+  	mobileNaVisible: false
+  }
+
+  this.state = {};
+}
+
+handleResize() {
+	this.setState({windowWidth: window.innerWidth});
+}
+
+componentDidMount() {
+	window.addEventListener('resize',this.handleResize.bind(this));
+}
+
+componentWillMount() {
+	window.removeEventListener('resize', this.handleResize.bind(this));
+}
+
+navigationLinks() {
+	return [
+      <ul>
+        <li key={1}> <Link to="about"> SOBRE </Link></li>
+      </ul>
+	]
+}
